ca03f70aa32001f04c07821a411419302077ae55
Sushankznation <sushantbharwalW@gmail.com>
2023-02-26 10:51:14 +0530

    Added API Key

---
+++ src/components/Images.jsx
@@ -0,0 +1,16 @@
+export default function Images() {
+  return (
+    <>
+        <h1 className="text-center px-2 mt-6 text-2xl">Results : </h1>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 my-10">
+        <h1>Img 1</h1>
+        <h1>Img 1</h1>
+        <h1>Img 1</h1>
+        <h1>Img 1</h1>
+        <h1>Img 1</h1>
+        <h1>Img 1</h1>
+        </div>
+
+    </>
+  )
+}
---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+export default function Search() {
+  return (
+    <div className="flex">
+        <input 
+        className="bg-gray-50 border border-gray-300 text-sm w-full indent-2 p-2.5 outline-none focus:border-blue-500 focus:ring-2 rounded-tl rounded-bl"
+        type="search" 
+            placeholder="Type here..."
+        />
+        <button className="bg-blue-600 px-6 py=2.5 text-white rounded-tr rounded-br focus:ring-2 focus:ring-blue-300 disabled:bg-gray-400">Search</button>
+    </div>
+  )
+}
